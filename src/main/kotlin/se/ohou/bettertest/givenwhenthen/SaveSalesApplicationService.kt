299b976c21cbc29b6dda23e134a94e849e5b6169
ohouse-willy <jeonghun.kim@bucketplace.net>
2022-06-15 23:49:28 +0900

    method 입/출력값 작성, when 절에 호출부분 채워넣기

--- src/main/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationService.kt
+++ src/main/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationService.kt
@@ -1,4 +1,10 @@
 package se.ohou.bettertest.givenwhenthen
 
 class SaveSalesApplicationService {
+
+    fun updateSalesApplication(command: UpdateCommand) {}
+
+    data class UpdateCommand(
+        val id: Long
+    )
 }
--- src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
+++ src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
@@ -1,20 +1,27 @@
 package se.ohou.bettertest.givenwhenthen
 
 import io.kotest.core.spec.style.BehaviorSpec
+import se.ohou.bettertest.givenwhenthen.SaveSalesApplicationService.UpdateCommand
 
 class SaveSalesApplicationServiceTest : BehaviorSpec({
 
     val saveSalesApplicationService = SaveSalesApplicationService()
 
     Given("validation 을 통과하는 상품판매신청서 등록 Command 가 있다.") {
+        val validCommand = UpdateCommand(
+            id = 1,
+        )
+
         And("DB 에서 업데이트를 하기 위한 Record 를 찾을 수 있다") {
             When("상품판매신청서 수정이 실행되면") {
+                saveSalesApplicationService.updateSalesApplication(validCommand)
                 Then("DB에 정상적으로 업데이트 된다") {
                 }
             }
         }
         And("And: DB 에서 업데이트를 하기 위한 Record 를 찾을 수 없다.") {
             When("상품판매신청서 수정이 실행되면") {
+                saveSalesApplicationService.updateSalesApplication(validCommand)
                 Then("NoSuchElementException 이 발생한다") {
                 }
             }
@@ -22,8 +29,12 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
     }
 
     Given("validation 을 통과하지 못하는 상품판매신청서 등록 Command 가 있다.") {
+        val notValidCommand = UpdateCommand(
+            id = 1,
+        )
         And("DB 에서 업데이트를 하기 위한 Record 를 찾을 수 있다.") {
             When("상품판매신청서 수정이 실행되면") {
+                saveSalesApplicationService.updateSalesApplication(notValidCommand)
                 Then("Validation Error 가 발생한다") {
                 }
             }
