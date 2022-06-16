98a3864e80dbc947d872cc77e92ac943e0ff3ecf
ohouse-willy <jeonghun.kim@bucketplace.net>
2022-06-16 15:13:10 +0900

    SalesApplicationValidator, SalesAppliationPort interface 추가

--- src/main/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationService.kt
+++ src/main/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationService.kt
@@ -1,10 +1,29 @@
 package se.ohou.bettertest.givenwhenthen
 
-class SaveSalesApplicationService {
+class SaveSalesApplicationService(
+    private val salesApplicationValidator: SalesApplicationValidator,
+    private val salesApplicationPort: SalesApplicationPort,
+) {
 
     fun updateSalesApplication(command: UpdateCommand) {}
 
     data class UpdateCommand(
-        val id: Long
+        val id: Long,
+        val content: String,
     )
+}
+
+data class SalesApplication(
+    val id: Long,
+    val content: String,
+)
+
+interface SalesApplicationValidator {
+    @Throws(IllegalArgumentException::class)
+    fun valid(command: SaveSalesApplicationService.UpdateCommand)
+}
+
+interface SalesApplicationPort {
+    fun findById(id: Long): SalesApplication?
+    fun update(salesApplication: SalesApplication): SalesApplication
 }
--- src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
+++ src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
@@ -1,15 +1,23 @@
 package se.ohou.bettertest.givenwhenthen
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.mockk
 import se.ohou.bettertest.givenwhenthen.SaveSalesApplicationService.UpdateCommand
 
 class SaveSalesApplicationServiceTest : BehaviorSpec({
 
-    val saveSalesApplicationService = SaveSalesApplicationService()
+    val validator = mockk<SalesApplicationValidator>()
+    val port = mockk<SalesApplicationPort>()
+    val saveSalesApplicationService = SaveSalesApplicationService(
+        salesApplicationValidator = validator,
+        salesApplicationPort = port,
+    )
 
     Given("validation 을 통과하는 상품판매신청서 등록 Command 가 있다.") {
+        val id = 1L
         val validCommand = UpdateCommand(
-            id = 1,
+            id = id,
+            content = "new"
         )
 
         And("DB 에서 업데이트를 하기 위한 Record 를 찾을 수 있다") {
@@ -29,8 +37,10 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
     }
 
     Given("validation 을 통과하지 못하는 상품판매신청서 등록 Command 가 있다.") {
+        val id = 1L
         val notValidCommand = UpdateCommand(
-            id = 1,
+            id = id,
+            content = "new"
         )
         And("DB 에서 업데이트를 하기 위한 Record 를 찾을 수 있다.") {
             When("상품판매신청서 수정이 실행되면") {
