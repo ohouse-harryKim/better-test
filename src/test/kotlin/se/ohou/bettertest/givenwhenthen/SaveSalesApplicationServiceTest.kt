03fcbd4d95a6e58da53dd1c71482fa7d4500adb9
ohouse-willy <jeonghun.kim@bucketplace.net>
2022-06-16 12:13:33 +0900

    test 케이스 작성

--- src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
+++ src/test/kotlin/se/ohou/bettertest/givenwhenthen/SaveSalesApplicationServiceTest.kt
@@ -1,11 +1,15 @@
 package se.ohou.bettertest.givenwhenthen
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
 import io.mockk.slot
+import io.mockk.verify
 import se.ohou.bettertest.givenwhenthen.SaveSalesApplicationService.UpdateCommand
 
 class SaveSalesApplicationServiceTest : BehaviorSpec({
@@ -17,16 +21,22 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
         salesApplicationPort = port,
     )
 
+    afterContainer {
+        clearAllMocks()
+    }
+
     Given("validation 을 통과하는 상품판매신청서 등록 Command 가 있다.") {
         val id = 1L
         val validCommand = UpdateCommand(
             id = id,
-            content = "new"
+            content = "new",
         )
 
-        every {
-            validator.valid(any())
-        } just runs
+        beforeContainer {
+            every {
+                validator.valid(any())
+            } just runs
+        }
 
         And("DB 에서 업데이트를 하기 위한 Record 를 찾을 수 있다") {
 
@@ -42,7 +52,11 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
                 } answers { slot.captured }
 
                 saveSalesApplicationService.updateSalesApplication(validCommand)
+
                 Then("DB에 정상적으로 업데이트 된다") {
+                    verify(exactly = 1) { port.update(any()) }
+                    slot.captured.id shouldBe 1L
+                    slot.captured.content shouldBe "new"
                 }
             }
         }
@@ -53,8 +67,11 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
             } returns null
 
             When("상품판매신청서 수정이 실행되면") {
-                saveSalesApplicationService.updateSalesApplication(validCommand)
                 Then("NoSuchElementException 이 발생한다") {
+                    shouldThrow<NoSuchElementException> {
+                        saveSalesApplicationService.updateSalesApplication(validCommand)
+                    }
+                    verify(inverse = true) { port.update(any()) }
                 }
             }
         }
@@ -72,8 +89,11 @@ class SaveSalesApplicationServiceTest : BehaviorSpec({
         } throws IllegalArgumentException()
 
         When("상품판매신청서 수정이 실행되면") {
-            saveSalesApplicationService.updateSalesApplication(notValidCommand)
             Then("Validation Error 가 발생한다") {
+                shouldThrow<IllegalArgumentException> {
+                    saveSalesApplicationService.updateSalesApplication(notValidCommand)
+                }
+                verify(inverse = true) { port.update(any()) }
             }
         }
     }
